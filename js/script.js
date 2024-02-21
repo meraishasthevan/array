86edcc67b73b85516a8fa106e117fade732dd7d2
merisha <meraishastevan@gmail.com>
2024-02-21 09:39:39 +0530

    array file

---
+++ js/script.js
@@ -0,0 +1,69 @@
+/*separate even number in array*/
+
+  let a=[5,76,3,675,2,3,7];
+  let num=[];
+  for(let i=0; i<a.length; i++){
+	  if(a[i]%2==0){
+		num.push(a[i]);
+	  }
+  }
+  console.log(num);
+  
+ /*print the numbers*/
+ 
+  let b=[22,5,32,78,99,23,66];
+  let num1=[];
+  for(let i=0; i<b.length; i++){
+	  if(b[i]%11==0){
+		  num1.push(b[i]);
+	  }
+  } 
+  console.log(num1);  
+
+ /*find the output*/
+ 
+  let c=[1,2,3,4,5];
+  let sum=0;
+  for(let i=0; i<c.length; i++){
+	sum+=c[i];
+	
+ }
+ console.log(sum);
+ 
+ /*find the output*/
+ 
+ let d=[1,2,3,4,5];
+ let count=0;
+ for(let i=0; i<d.length; i++){
+	count+=d[i];
+ }
+ console.log(count/d.length);
+ 
+ /*find the output*/
+ 
+ let e=[2,4,7,9,12,3]
+ let sum1=0;
+ for(let i=0; i<e.length; i++){
+	if(e[i]%2==0){
+	 sum1+=e[i];
+	}
+ }
+ console.log(sum1);
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
